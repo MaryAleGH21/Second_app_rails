5cb4c49e74726e740c6bd1a0be97ea15560baf65
Alejandra <marialesr.17@gmail.com>
2021-02-27 10:02:25 -0600

    Finished proyect

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'users/login'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get  'login', to: 'users#login'
+  get  'about_us', to: 'pages#about_us'
+  get  'test', to: 'pages#test'
+  root 'pages#index'
+end
